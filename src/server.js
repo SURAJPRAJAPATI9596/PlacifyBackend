50141b904e4cbcc40b4aa901df6b8c3744763589
SURAJPRAJAPATI9596 <SURAJPRAJAPATIJOB@GMAIL.COM>
2026-07-11 18:40:55 +0530

    changed url for testing

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = require("./app");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 8080;
 connectDB();
+
 app.listen(port, () => {
   console.log("Placify server started");
 });
